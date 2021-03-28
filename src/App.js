3538cf11598a5beaead8eb437605db84878934c1
Rafael Martins <rafael.martins92@outlook.com>
2021-03-28 03:56:05 -0300

    feat: integrating redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import "./App.css";
 
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Feed from "./components/Feed/Feed";
+import Login from "./components/Login/Login";
+import { selectUser } from "./features/userSlice";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
       <Header />
-      {/* App Body */}
-      <div className="app-body">
-        <Sidebar />
-        <Feed />
-        {/* Widdgets */}
-      </div>
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app-body">
+          <Sidebar />
+          <Feed />
+          {/* Widdgets */}
+        </div>
+      )}
     </div>
   );
 }
